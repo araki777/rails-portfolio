846f00fe394729db8423ecae3ba1bfcb523db3f6
araki777 <arakichi777@icloud.com>
2020-10-04 16:46:02 +0900

    config-false

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.hosts << "engineer-meets.herokuapp.com"
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -58,5 +59,5 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 end
